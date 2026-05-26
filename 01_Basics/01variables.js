6dab20cec300abedf15b1455b31bdd4fff6937d1
KaifShaikh98 <kaifshaikh1942004@gmail.com>
2026-05-26 19:38:22 +0000

    Variables and Datatypes

---
+++ 01_Basics/01variables.js
@@ -0,0 +1,16 @@
+const accountId=14553
+let accountEmail="Kaif@google.com"
+var accountCity="Pune"
+let accountState
+
+
+
+
+accountEmail="KL@gmail.com"
+
+
+console.table([accountEmail,accountId,accountCity,accountState])
+
+/*Prefer Not use var 
+because  of issue in block scope and and functional scope 
+*/
---
+++ 01_Basics/02Datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS code as newer version
+
+
+//alert(3+3)  //We are using Nodejs Not Browser
+
+console.log();
+
+
+// Datatype in Javascript 
+let name="Kaif"
+let age=18
+let isLoggedIn=false
+let state;   //undefined beacuse we cant inititalize variable to value 
+
+
+//primitve Data Types
+//number  => 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+// null => standalone value
+//undefined =>
+//symbol  => unique  
+
+
+//object
+
+console.log(typeof("Kaif"))  //string
+console.log(typeof(age))    //number because we initilize 18 on above side
+console.log(typeof(null))   //datatype object
+
+console.log(typeof(undefined))  // undefined
+console.table([age,null])
+
