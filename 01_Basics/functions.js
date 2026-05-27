7805f78c26df111218dd04c83f93463ac38b0099
KaifShaikh98 <kaifshaikh1942004@gmail.com>
2026-05-27 09:28:45 +0000

    functions

---
+++ 01_Basics/functions.js
@@ -0,0 +1,4 @@
+arr=[5,6,8,9]
+
+arr.unshift(1,2,3,4,5)// Adding element from starting of index directly insert element from 0 index
+console.log("Original Array:",arr);  //output:[1,2,3,4,5,5,6,8,9]
